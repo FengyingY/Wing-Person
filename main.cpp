febbe5dd218f4594ec278d78daa85905bc0c856e
FengyingY <fengyingyoung@gmail.com>
2020-10-06 23:49:02 -0400

    updated README

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char **argv) {
 
 	//create window:
 	SDL_Window *window = SDL_CreateWindow(
-		"gp20 game4: choice-based game", //TODO: remember to set a title for your game!
+		"Mario's", //TODO: remember to set a title for your game!
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		800, 600, //TODO: modify window size if you'd like
 		SDL_WINDOW_OPENGL
